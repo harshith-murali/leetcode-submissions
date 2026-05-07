18f679c2313dada83a53c45cbb082a38c60907df
Harshith M <mharshith200@gmail.com>
2026-05-07 12:34:31 +0530

    Time: 0 ms (100%), Space: 15 MB (60.86%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    int f(int i, int j , vector<vector<int>>& mat,vector<vector<int>>& dp){
-        int n = mat.size();
-        if(j < 0 || j >= n) return 1e9;
-        if(i==n-1) return mat[i][j];
-        if(dp[i][j] != 1e9) return dp[i][j];
-        return dp[i][j] = mat[i][j] + min(
-            f(i+1,j,mat,dp),
-            min(
-                f(i+1,j+1 , mat, dp),
-                f(i+1,j-1 , mat , dp)
-                )
-            );
-    }
     int minFallingPathSum(vector<vector<int>>& mat) {
+        
         int n = mat.size();
-        vector<vector<int>> dp(n, vector<int>(n,1e9));
-        int mini = INT_MAX;
-        for(int j=0; j<n; j++){
-            mini = min(mini, f(0,j,mat,dp));
+
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for(int j = 0; j < n; j++) {
+            dp[n - 1][j] = mat[n - 1][j];
         }
+
+        for(int i = n - 2; i >= 0; i--) {
+
+            for(int j = 0; j < n; j++) {
+
+                int down = dp[i + 1][j];
+
+                int leftDiagonal = 1e9;
+                if(j > 0)
+                    leftDiagonal = dp[i + 1][j - 1];
+
+                int rightDiagonal = 1e9;
+                if(j < n - 1)
+                    rightDiagonal = dp[i + 1][j + 1];
+
+                dp[i][j] = mat[i][j] +
+                           min(down,
+                           min(leftDiagonal, rightDiagonal));
+            }
+        }
+
+        int mini = INT_MAX;
+
+        for(int j = 0; j < n; j++) {
+            mini = min(mini, dp[0][j]);
+        }
+
         return mini;
     }
 };
