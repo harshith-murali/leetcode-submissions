483976aa6762ba214439d0fccea2449131f6bb96
Harshith M <mharshith200@gmail.com>
2026-05-27 18:56:26 +0530

    Time: 3 ms (61.15%), Space: 10.2 MB (57.9%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     bool isSafe(vector<string>& board, int row, int col, int n) {
-        // horizontal : row from 0->n i->n
-        for (int j = 0; j < n; j++) {
-            if (board[row][j] == 'Q')
-                return false;
-        }
 
-        // vertical : col from 0->n , j->n
-        for (int i = 0; i < n; i++) {
-            if (board[i][col] == 'Q')
-                return false;
-        }
-
-        // left diagonal
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-        // right diagonal
-        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-        return true;
+    for(int i = row - 1; i >= 0; i--) {
+        if(board[i][col] == 'Q') return false;
     }
+
+    for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+        if(board[i][j] == 'Q') return false;
+    }
+
+    for(int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+        if(board[i][j] == 'Q') return false;
+    }
+
+    return true;
+}
     void nQueens(int row, int n, vector<vector<string>>& ans,
                  vector<string>& board) {
         if (row == n) {
