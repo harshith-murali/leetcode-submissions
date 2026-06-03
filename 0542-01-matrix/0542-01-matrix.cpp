3c761da037f1d19426b8cc2cb72e31618415a719
Harshith M <mharshith200@gmail.com>
2026-06-03 21:12:40 +0530

    Time: 45 ms (10.72%), Space: 38.9 MB (29.68%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isValidCell(int currRow , int currCol , int m , int n){
+        return currRow >= 0 && currRow < m && currCol >= 0 && currCol < n;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> vis(m , vector<int>(n,0));
+        vector<vector<int>> dist(m , vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 0){
+                    vis[i][j] = 1;
+                    q.push({{i,j},0});
+                }else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int delrow[] = {-1,0,+1,0};
+        int delcol[] = {0,+1,0,-1};
+
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+            dist[row][col] = steps;
+
+            for(int i=0; i<4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(isValidCell(nrow, ncol , m , n) && vis[nrow][ncol] == 0){
+                    q.push({{nrow,ncol} , steps+1});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+        return dist;
+
+    }
+};
