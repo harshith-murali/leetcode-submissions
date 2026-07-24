91a09957c2a02588838c432d3f7a67d1b0729485
Harshith M <mharshith200@gmail.com>
2026-07-24 20:38:28 +0530

    Time: 578 ms (8.2%), Space: 55.7 MB (7.4%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool f(int idx, vector<int>& nums, vector<int>&dp) {
+        if (idx >= nums.size() - 1)
+            return true;
+        if(dp[idx] != -1) return dp[idx];
+        for (int i = 1; i <= nums[idx]; i++) {
+            if (f(idx + i, nums,dp)) {
+                return dp[idx] = true;
+            }
+        }
+        return dp[idx] = false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size() , -1);
+         return f(0, nums,dp); 
+    }
+};
