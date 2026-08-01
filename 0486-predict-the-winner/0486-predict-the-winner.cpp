6158bda2070151eea20d88d5e9d69ca95d7b9b21
Harshith M <mharshith200@gmail.com>
2026-08-01 22:57:51 +0530

    Time: 2 ms (43.58%), Space: 10.2 MB (32.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int i , int j , vector<int>& nums, vector<vector<int>> &dp){
+        if(i > j) return 0;
+
+        if(i == j) return nums[i];
+        if(dp[i][j] != -1) return dp[i][j];
+        int take_i = nums[i] - solve(i+1,j,nums,dp);
+        int take_j = nums[j] - solve(i,j-1,nums,dp);
+
+        return dp[i][j]= max(take_i , take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n , vector<int>(n,-1));
+        return solve(0, n-1, nums, dp) >= 0;
+    }
+};
