22bcdcacd6b6fbdfb6e39152f0cf4185d0cf39a6
Harshith M <mharshith200@gmail.com>
2026-08-28 08:59:19 +0530

    Time: 50 ms (45.28%), Space: 33.1 MB (13.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -25,7 +25,7 @@ public:
 
         sort(begin(nums) , end(nums));
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n-2; i++){
             if(i>0&& nums[i] == nums[i-1]) continue;
 
             int n1 = nums[i];
